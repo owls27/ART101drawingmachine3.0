d15b48c7dd878c2987c1c0dbcd322b1fe51dd1e8
owls27 <tzuhyo0927@gmail.com>
2021-11-07 01:37:09 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
 function setup() {
   createCanvas(400, 400);
+  background(60, 8, 82);
+  strokeWeight(3);
 }
 
 function draw() {
-  background(220);
+  //if (mouseIsPressed == true){
+  //}else {}
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 0, 600, 255, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+
+
+
 }
