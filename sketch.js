123f7bffef09a4c9f8104b6ba2f7c0459d32b22b
owls27 <tzuhyo0927@gmail.com>
2021-11-15 01:36:04 -0800

    UPDATED

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let array = [];
+
 function setup() {
   createCanvas(400, 400);
   background(60, 8, 82);
@@ -8,16 +10,20 @@ function draw() {
   //if (mouseIsPressed == true){
   //}else {}
   if (mouseIsPressed) {
-    stroke(map(mouseX, 0, 600, 0, 255, true));
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    //stroke(map(mouseX, 0, 600, 0, 255, true));
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
     line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, ,mouseY]);
   }
 }
 
-function keyTyped(){
-  if (key === 's'){
+function keyTyped() {
+  if (key === 's') {
     //save this image
     saveCanvas('fileName', 'png');
+  } else if (key === 'd') {
+    //display image
+    line(array[0][0], array[0][1], array[1][0], array[1][1]);
   }
   return false;
 }
